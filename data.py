87e794c15b4308eba33d02fa26022f79d2d85aed
Artem <lunartem@yandex.ru>
2019-11-06 18:54:47 +0200

    [+] blog test app

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article one',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quo quibusdam officiis maxime ipsam dolore nobis dolorum aspernatur nostrum est voluptate asperiores, eligendi praesentium! Perferendis quaerat doloribus distinctio in at!',
+            'author':'user',
+            'create_date':'6-11-2019'     
+        },
+        {
+            'id':2,
+            'title':'Article Two',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quo quibusdam officiis maxime ipsam dolore nobis dolorum aspernatur nostrum est voluptate asperiores, eligendi praesentium! Perferendis quaerat doloribus distinctio in at!',
+            'author':'Foo',
+            'create_date':'6-11-2019'     
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quo quibusdam officiis maxime ipsam dolore nobis dolorum aspernatur nostrum est voluptate asperiores, eligendi praesentium! Perferendis quaerat doloribus distinctio in at!',
+            'author':'Pisos',
+            'create_date':'6-11-2019'     
+        }
+
+    ]
+    return articles
